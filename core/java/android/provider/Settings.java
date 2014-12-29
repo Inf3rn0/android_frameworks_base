0a60b1aecc2d5213aec80ac2d4a510d0a208a3c8
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    Fw_base - AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3562,8 +3562,6 @@ public final class Settings {
         public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
 
         /**
-         * The alpha value of the On-The-Go overlay.
-         *
          * @hide
          */
         public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
